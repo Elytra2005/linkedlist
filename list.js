54b312b7be199406e1f8710e33d81e81ad25bcce
Mahmoud El Hajj <86180377+Elytra2005@users.noreply.github.com>
2025-09-05 16:05:27 -0400

    Update list.js

--- list.js
+++ list.js
@@ -1,45 +1,54 @@
 class node {
     constructor(data) {
-        // the value inside the node
         this.data = data;
-        // finds what value comes after
-        this.next = null;
+        this.next = null; // will have a inner node
     }
 }
 
+
 class linkedList {
     constructor() {
-        // our first node 
         this.head = null;
         this.length = 0;
     }
     
     
+    // the data is pretty much the value we pass
     append(data) {
-        // this will refrence the other class like .next and .data
         const node_ = new node(data);
-        
         if(this.head == null) {
-            // if theres no head currently do this
-            // the head now itself is a node and points to the node obj
             this.head = node_;
         } else {
-            // if its already initalized
-            while(this.head != null) {
-                this.head = this.head.next;
+            let current = this.head;
+            // 
+            while(current.next) {
+                current = current.next;
             }
-            this.head = node_
+            current.next = node_;
         }
-         this.length++;
+        
+        this.length++;
     }
     
-   
+    
+    printList() {
+        
+    }
+    
+    removeNode() {
+        
+    }
+    
+    addNode() {
+        
+    }
 }
+
 
 let list = new linkedList();
 
 list.append(5);
 list.append(10);
-
+list.append(20);
 
 console.log(list);
